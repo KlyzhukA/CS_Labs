d5b1c287914f1b6bcc1b463d651c3c0764444254
Alexander <klyzhuk_as_21@mf.grsu.by>
2025-10-02 13:56:43 +0300

    exception

--- CS_Lab2/Program.cs
+++ CS_Lab2/Program.cs
@@ -6,25 +6,32 @@ class Program
     {
         string output = "C:\\Users\\klyzh\\source\\repos\\CS_Lab1\\CS_Lab2\\Files\\PursuitLog.txt";
         string input = "";
-        Console.WriteLine("Выберите файл:\n" +
-            "1 1.ChaseData\n" +
-            "2 2.ChaseData\n" +
-            "3 3.ChaseData");
-        int choice = Convert.ToInt32(Console.ReadLine());
-        switch (choice)
+        try
         {
-            case 1:
-                input = "C:\\Users\\klyzh\\source\\repos\\CS_Lab1\\CS_Lab2\\Files\\1.ChaseData.txt";
-                break;
-            case 2:
-                input = "C:\\Users\\klyzh\\source\\repos\\CS_Lab1\\CS_Lab2\\Files\\2.ChaseData.txt";
-                break;
-            case 3:
-                input = "C:\\Users\\klyzh\\source\\repos\\CS_Lab1\\CS_Lab2\\Files\\3.ChaseData.txt";
-                break;
+            Console.WriteLine("Выберите файл:\n" +
+                "1 1.ChaseData\n" +
+                "2 2.ChaseData\n" +
+                "3 3.ChaseData");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    input = "C:\\Users\\klyzh\\source\\repos\\CS_Lab1\\CS_Lab2\\Files\\1.ChaseData.txt";
+                    break;
+                case 2:
+                    input = "C:\\Users\\klyzh\\source\\repos\\CS_Lab1\\CS_Lab2\\Files\\2.ChaseData.txt";
+                    break;
+                case 3:
+                    input = "C:\\Users\\klyzh\\source\\repos\\CS_Lab1\\CS_Lab2\\Files\\3.ChaseData.txt";
+                    break;
 
+            }
+            Game game = new Game(input, output);
+            game.Run();
         }
-        Game game = new Game(input,output);
-        game.Run();
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
     }
 }
