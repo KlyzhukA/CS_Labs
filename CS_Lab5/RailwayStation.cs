5f7a174e15bec0e96fe99e7b9c861b73287d2139
Admin <klyzhuk.anton.007@gmail.com>
2025-12-15 14:28:58 +0300

    station + user

---
+++ CS_Lab5/RailwayStation.cs
@@ -0,0 +1,49 @@
+﻿namespace CS_Lab5
+{
+    public class RailwayStation
+    {
+        public string StationName { get; set; }
+        public string City { get; set; }
+        public List<Train> Trains { get; set; } = new List<Train>();
+
+        public List<PassengerTrain> GetAvailablePassengerTrains()
+        {
+            var now = DateTime.Now;
+            var availableTrains = new List<PassengerTrain>();
+
+            foreach (var train in Trains)
+            {
+                if (train is PassengerTrain passengerTrain)
+                {
+                    if (passengerTrain.DepartureTime > now &&
+                        passengerTrain.GetAvailableSeats() > 0)
+                    {
+                        availableTrains.Add(passengerTrain);
+                    }
+                }
+            }
+
+            return availableTrains;
+        }
+
+        public List<CargoTrain> GetAvailableCargoTrains()
+        {
+            var now = DateTime.Now;
+            var availableTrains = new List<CargoTrain>();
+
+            foreach (var train in Trains)
+            {
+                if (train is CargoTrain cargoTrain)
+                {
+                    if (cargoTrain.DepartureTime > now &&
+                        cargoTrain.GetAvailableCapacity() > 0)
+                    {
+                        availableTrains.Add(cargoTrain);
+                    }
+                }
+            }
+
+            return availableTrains;
+        }
+    }
+}
---
+++ CS_Lab5/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_Lab5
+{
+    public abstract class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+
+    }
+}
